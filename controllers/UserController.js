9d8b76e2743ab9b0de263c2388d894df9b5ab7d2
Aldo Twizerimana <aldotwizerimana@Aldos-MacBook-Pro.local>
2024-04-22 15:09:14 +0200

    adding all users controller

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -24,11 +24,7 @@ export const getUser = async (req, res) => {
 export const getAllUsers = async (req, res) => {
 
   try {
-    let users = await UserModel.find();
-    users = users.map((user)=>{
-      const {password, ...otherDetails} = user._doc
-      return otherDetails
-    })
+    let users = await UserModel.find().find().select('-password').lean().exec();;
     res.status(200).json(users);
   } catch (error) {
     res.status(500).json(error);
